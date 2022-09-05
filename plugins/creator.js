5fa03f66b7be4d6b19793243433619ad3dc54a42
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-05 13:45:26 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn }) => {
   try {
     const sentMsg = await conn.sendContactArrayS(m.chat, [
-      [`${owner[0]}`, `${await conn.getName(owner[0] + '@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `raselcomel18@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://raselcomel.github.io/`, `👤 Gada pawang nih senggol dong 😣`],
-      [`${owner[1]}`, `${await conn.getName(owner[1] + '@s.whatsapp.net')}`, `👑 Developer Bot `, `📵 Don't spam/call me 😡`, `mr.familia13@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://instagram.com/aguzfamilia`, `👤 Hanya seseoarng biasa yang kadang butuh perhatian ☺`],
-      [`${owner[2]}`, `${await conn.getName(owner[2] + '@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `aniqshehyar1@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://github.com/aniq12`, `👤 Gada pawang nih senggol dong 😣`],
-      [`${owner[3]}`, `${await conn.getName(owner[3] + '@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `amirul@skyn.tech`, `🇮🇩 Indonesia`, `🚀 https://github.com/amiruldev20`, `👤 Sudah ada pawang banh 😣`]
+      [`${owner[0]}`, `${await conn.getName(owner[0] + '@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `arulltes@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg`, `👤 Tidak Dimanja Oleh Harta,Tetapi Dididik Beretika 😏`],
+      [`${owner[1]}`, `${await conn.getName(owner[1] + '@s.whatsapp.net')}`, `👑 Developer Bot `, `📵 Don't spam/call me 😡`, `arulltes@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg`, `👤 Hanya seseoarng biasa yang kadang butuh perhatian ☺`],
+      [`${owner[2]}`, `${await conn.getName(owner[2] + '@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `arulltes@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg`, `👤 Dingin Nih,Panasin Donk 😣`],
+      [`${owner[3]}`, `${await conn.getName(owner[3] + '@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `arulltes@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg`, `👤 Dingin Nih,Panasin Donk  😣`]
     ], m)
     await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, sentMsg, { mentions: [m.sender] })
   } catch {
